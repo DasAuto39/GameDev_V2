cf9a8c4d688ecfbadf398c6c91f67d83258241c5
DasAuto39 <akhrisgilang@gmail.com>
2024-05-20 08:22:50 +0700

    render a object

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+
+#include "RenderWindow.h"
+#include "Entity.h"
+#include "Map.h"
+
+int main(int argc, char** argv)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) > 0)
+    {
+        std::cout << "SDL_Init has failed. SDL_ERROR: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG)))
+    {
+        std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Initialize the window
+    RenderWindow window("GAME v1.0", 700, 640);
+    Map gameMap(&window);
+
+    // Load the texture for the entities
+    SDL_Texture* Batu = window.loadTexture("img_obj/1.png");
+    SDL_Texture* crates = window.loadTexture("img_obj/crate.png");
+    // Create an array of entities
+    Entity entities[4] =      {Entity(-32, 100, Batu),
+                              Entity(636, 100, Batu),
+                              Entity(-32, 400, Batu),
+                              Entity(636, 400, Batu)};
+
+    Entity Crates[4] =
+    {
+        Entity(50,54,crates),
+        Entity(344,30,crates),
+        Entity(255,520,crates),
+        Entity(200,400,crates)
+    };
+
+
+    bool gameRunning = true;
+    SDL_Event event;
+
+    while (gameRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                gameRunning = false;
+        }
+
+        window.clear();
+        gameMap.cldrawmap();
+
+        // Render all entities
+        for (int i = 0; i < 4; i++)
+        {
+            window.render(entities[i]);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            window.render(Crates[i]);
+        }
+        window.display();
+    }
+
+    window.cleanUp();
+    SDL_Quit();
+
+    return 0;
+}
---
+++ src/RenderWindow.h
@@ -0,0 +1,23 @@
+#ifndef RENDERWINDOW_H
+#define RENDERWINDOW_H
+
+
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "Entity.h"
+class RenderWindow
+{
+public:
+	RenderWindow(const char* p_title, int p_w, int p_h);
+    SDL_Texture* loadTexture(const char* p_filePath);
+	void render(Entity& p_entity);
+    void render(SDL_Texture* texture,SDL_Rect src, SDL_Rect dst);
+	void display();
+    void cleanUp();
+    void clear();
+private:
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
+#endif // RENDERWINDOW_H
